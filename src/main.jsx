87b89fd59d027099e01c1097e33213300bed818d
arevco <andy@revcopack.com>
2026-02-18 23:47:31 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import "./storage-shim.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AuthGate from "./AuthGate.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthGate>
+      <App />
+    </AuthGate>
   </React.StrictMode>
 );
